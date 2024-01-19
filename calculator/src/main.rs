45c4d1b56d0fe00f4f4cb780dbc3ff0205d1ec9f
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2024-01-19 20:38:01 +0300

    Подключение регуляÐрного выражения

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,5 +1,6 @@
 // CLI калькулятор
 use std::io;
+use regex::Regex;
 
 fn main(){
 
@@ -7,7 +8,7 @@ fn main(){
 
     loop {
         println!("Введите Ваше выражение(напр. 2 + 2)");
-        let input = String::new();
+        let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Ошибка чтения");
         let input = input.trim();
 
@@ -19,10 +20,21 @@ fn main(){
         println!("Результат вычисления: {}", result);
 
     }
-
     fn calculate(input: &str) -> f64 {
+        let res = Regex::new(r"([\d.]+)\s*([+-/*])\s*
+([\d.]+)").unwrap();
+        let captures = res.captures(input).unwrap();
+        let num1: f64 = captures[1].parse().unwrap();
+        let num2: f64 = captures[3].parse().unwrap();
+        let opt = &captures[2];
 
-        0.0
+        match opt {
+            "+" => num1 + num2,
+            "-" => num1 - num2,
+            "*" => num1 * num2,
+            "/" => num1 / num2,
+            _ => panic!("Неизвестная операция!"),
+        }
     }
 
 }
